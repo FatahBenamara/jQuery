fe86b907770d374e83df7c9474e5dcfb64f3e4ff
benamara <fatahbenamara87@hotmail.fr>
2020-07-05 23:53:28 +0200

    list

--- jr1/script.js
+++ jr1/script.js
@@ -1,1 +1,5 @@
-$("h1").html("hello");
+$(".logo").html("Nouvel exercice");
+$("#intro").html("utilisation des sélecteurs CSS pour faciliter la selection des balises HTML à modifier");
+// $("ul li:nth-child(2)").html("liste courses");
+$(".second").html("liste des course");
+$(".last").html("A ne pas oublier");
---
+++ jr2/liste.js
@@ -0,0 +1,8 @@
+let listeLi= $("li");
+console.log(listeLi);
+
+let first=$("li:nth-child(1)").html("ceci est un element Li");
+console.log(first);
+
+let last=$("li:nth-child(3)").html("ceci est le dernier element Li");
+console.log(last);
