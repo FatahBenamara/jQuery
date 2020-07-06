37b3fe8dac724349daaa7e9bf6085ea2a1b1a911
benamara <fatahbenamara87@hotmail.fr>
2020-07-06 23:59:33 +0200

    tables/selecteurs

---
+++ jr3/selecteurs/select.js
@@ -0,0 +1,5 @@
+$("ul li:nth-child(1)").html("jquery");
+$("ul li:nth-child(2)").html("bootstrap");
+$(".jour li").html("02/12/2019");
+$(".annee li:nth-child(1)").html("bonjour")
+$(".annee li:nth-child(5)").html("final")
